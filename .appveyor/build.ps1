3233772c7fce58fec63576082f0a066a2772bfcb
gael beard <gaelgael5@gmail.com>
2023-06-20 13:12:19 +0200

    test for windows

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -1,5 +1,11 @@
+
+# https://learn.microsoft.com/en-us/virtualization/windowscontainers/manage-docker/configure-docker-daemon#configure-docker-with-a-configuration-file
+# copy ./daemon.json C:\ProgramData\Docker\config\daemon.json
+
 # https://stefanscherer.github.io/use-appveyor-to-build-multi-arch-docker-image/
 # https://github.com/3shape/containerized-structure-test
+
+sc config docker binpath= "\"C:\Program Files\docker\dockerd.exe\" --run-service -H tcp://0.0.0.0:2375"
 
 # Variables to manage initialize in this script
 $imageName = "parrot";    # name of the image
