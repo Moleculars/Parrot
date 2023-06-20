f0df288ae6bc617d7be670ca6e80fe3df8fae943
gael beard <gaelgael5@gmail.com>
2023-06-20 07:21:20 +0200

    fix ubountu build

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -10,6 +10,9 @@ $imageName = "parrot";    # name of the image
 $ErrorActionPreference = 'Stop';
 
 Write-Host Starting build
+
+Set-Location .\Src\Black.Beard.Parrot\Black.Beard.ParrotServices
+
 if ($isWindows) 
 {
 
@@ -17,13 +20,13 @@ if ($isWindows)
   # error during connect: Post http:////./pipe/docker_engine/v1.40/build?buildargs={}&cachefrom=[]&cgroupparent=&cpuperiod=0&cpuquota=0&cpusetcpus=&cpusetmems=&cpushares=0&dockerfile=Dockerfile.windows&labels=%7B%7D&memory=0&memswap=0&networkmode=default&pull=1&rm=1&session=pv0909b63j0jxsnv1pvzo2y1s&shmsize=0&t=parrot&target=&ulimits=null&version=1: open //./pipe/docker_engine: 
   # The system cannot find the file specified. In the default daemon configuration on Windows, the docker client must be run elevated to connect. This error may also indicate that the docker daemon is not running.
   
-    Set-Location .\Src\Black.Beard.Parrot\Black.Beard.ParrotServices
-    docker build --pull -t $imageName -f Dockerfile.windows
+    docker build -t $imageName -f Dockerfile.windows
 
 } else 
 {
   # docker pull golang
-  docker build --platform "linux/$env:ARCH" -t $imageName # --build-arg "arch=$env:ARCH" .
+  # docker build --platform "linux/$env:ARCH" -t $imageName # --build-arg "arch=$env:ARCH" .
+  docker build -t $imageName .
 }
 
 docker images
